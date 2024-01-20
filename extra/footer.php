abb6fafed8ec3e3b2c8bcc529698c6270a040141
FavoShane <93580053+FavoShane@users.noreply.github.com>
2024-01-20 19:50:55 +0100

    Update footer.php

--- extra/footer.php
+++ extra/footer.php
@@ -1,4 +1,4 @@
 <div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.jss" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.jss"></script>
 
 </div>
